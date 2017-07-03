e5e4782bc39a485ed708673bb227be5a68ff922b
m.yudina <m.yudina@smarde.com>
2017-07-03 18:49:49 +0300

    server.go init

---
+++ server.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"os"
+	"io"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	//e.POST("/users", saveUser)
+	e.GET("/users/:id", getUser)
+	//e.PUT("/users/:id", updateUser)
+	//e.DELETE("/users/:id", deleteUser)
+	e.GET("/show", show)
+	e.POST("/save", save)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// e.GET("/users/:id", getUser)
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+//e.GET("/show", show)
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
+}
+
+// curl -F "name=Joe Smith" -F "avatar=@/path/to/your/avatar.png" http://localhost:1323/save
+// e.POST("/save", save)
+func save(c echo.Context) error {
+	// Get name
+	name := c.FormValue("name")
+	// Get avatar
+	avatar, err := c.FormFile("avatar")
+	if err != nil {
+		return err
+	}
+
+	// Source
+	src, err := avatar.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create(avatar.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.HTML(http.StatusOK, "<b>Thank you! " + name + "</b>")
+}
